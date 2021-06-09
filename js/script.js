1bd3166b273b7302561a60140b71395925914e8a
Jacob Strong <rjts2896@gmail.com>
2021-06-08 19:27:00 -0700

    add add, subtract, multiply, and divide functions, and business logic

--- js/script.js
+++ js/script.js
@@ -14,16 +14,16 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter A Number"));
-const number2 = parseInt(prompt("Enter Another Number"));
-const result = add(number1, number2);
+const boop1 = parseInt(prompt("Enter A Number"));
+const boop2 = parseInt(prompt("Enter Another Number"));
+const result = add(boop1, boop2);
 alert(result + ", ");
 
-const result2 = subtract(number1, number2);
+const result2 = subtract(boop1, boop2);
 alert(result2 + ", ");
 
-const result3 = multiply(number1, number2);
+const result3 = multiply(boop1, boop2);
 alert(result3 + ", ");
 
-const result4 = divide(number1, number2);
+const result4 = divide(boop1, boop2);
 alert(result4);
